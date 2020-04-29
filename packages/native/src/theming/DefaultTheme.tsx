5bdb95c56953fc62223c9793b1d500bea40bc405
Razz Fox <razzintown@gmail.com>
2020-04-28 17:14:20 -0700

    match iOS systemGray6 as default background color
    systemGray6 from https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/color/#system-colors

--- packages/native/src/theming/DefaultTheme.tsx
+++ packages/native/src/theming/DefaultTheme.tsx
@@ -4,7 +4,7 @@ const DefaultTheme: Theme = {
   dark: false,
   colors: {
     primary: 'rgb(0, 122, 255)',
-    background: 'rgb(242, 242, 242)',
+    background: 'rgb(242, 242, 247)',
     card: 'rgb(255, 255, 255)',
     text: 'rgb(28, 28, 30)',
     border: 'rgb(224, 224, 224)',
